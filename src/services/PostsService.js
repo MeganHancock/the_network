5c02111e471bbe9210c9caf8f9e56405ccff497f
MeganHancock <mgnhnck@gmail.com>
2024-03-01 10:57:19 -0700

    mapped posts, stored in appstate

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -1,12 +1,17 @@
 import { api } from "./AxiosService.js"
 import { logger } from "../utils/Logger.js"
+import { Post } from "../models/Post.js"
+import { AppState } from "../AppState.js"
 
 class PostsService {
 
 
     async getPosts() {
         const response = await api.get('api/posts')
-        logger.log('posts response', response.data.posts)
+        // logger.log('posts response', response.data.posts)
+        const foundPosts = response.data.posts.map(postPOJO => new Post(postPOJO))
+        // logger.log('posts response mapped', foundPosts)
+        AppState.posts = foundPosts
     }
 }
 
