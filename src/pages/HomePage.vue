1a928dc8d0c0153e53e63bc375347a82b403d2d9
MeganHancock <mgnhnck@gmail.com>
2024-03-01 11:03:41 -0700

    raw data dump

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,21 @@
-<template></template>
+<template>
+  <div class="container">
+    <section class="row">
+      <div v-for="post in posts" class="col-12 border border-secondary m-3 p-3 rounded-3 shadow">
+
+        {{ post }}
+
+      </div>
+    </section>
+  </div>
+</template>
 
 <script>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { postsService } from '../services/PostsService.js';
 import Pop from '../utils/Pop.js';
 import { logger } from '../utils/Logger.js';
+import { AppState } from "../AppState.js"
 
 export default {
   setup() {
@@ -22,7 +33,9 @@ export default {
       getPosts()
     })
 
-    return {}
+    return {
+      posts: computed(() => AppState.posts)
+    }
   }
 }
 </script>
